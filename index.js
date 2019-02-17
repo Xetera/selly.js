c2cf1e3f01380c06fa11c83dcdd731091c057445
Xetera <mythologyali@gmail.com>
2019-02-17 01:20:33 -0800

    cleaned up index

--- index.js
+++ index.js
@@ -11,14 +11,12 @@ const del = generate("delete");
  * data to be sent with a specific key, this makes it
  * easier on the user to not have to redundantly post it
  * under the key
- * @param key
- * @return {{body: boolean, key?: string}}
  */
-const withBody = key => ({ body: true, key });
+const withBody = key => ({ data: true, key });
 
 const generateMutation = method => (url, opts = {}) => ({
-	...generate(method)(url),
-	...withBody(opts.key)
+  ...generate(method)(url),
+  ...withBody(opts.key)
 });
 
 const post = generateMutation("post");
@@ -27,109 +25,96 @@ const put = generateMutation("put");
 
 
 const methods = {
-	getCoupons: get("/coupons"),
-	getCoupon: get("/coupon/:id"),
-	createCoupon: post("/coupons", { key: "coupon" }),
-	updateCoupon: put("/coupons/:id", { key: "coupon" }),
-	getOrders: get("/orders"),
-	getOrder: get("/orders/:id"),
-	getProducts: get("/products"),
-	getProduct: get("/products/:id"),
-	createProduct: post("/products", { key: "product" }),
-	updateProduct: put("/products/:id", { key: "product" }),
-	deleteProduct: del("/products/:id"),
-	getProductGroups: get("/product_groups"),
-	getProductGroup: get("/product_groups/:id"),
-	getQueries: get("/queries"),
-	getQuery: get("/queries/:id"),
-	createPayment: post("/pay"),
-	deletePayment: del("/pay/:id")
+  getCoupons: get("/coupons"),
+  getCoupon: get("/coupon/:id"),
+  createCoupon: post("/coupons", { key: "coupon" }),
+  updateCoupon: put("/coupons/:id", { key: "coupon" }),
+  getOrders: get("/orders"),
+  getOrder: get("/orders/:id"),
+  getProducts: get("/products"),
+  getProduct: get("/products/:id"),
+  createProduct: post("/products", { key: "product" }),
+  updateProduct: put("/products/:id", { key: "product" }),
+  deleteProduct: del("/products/:id"),
+  getProductGroups: get("/product_groups"),
+  getProductGroup: get("/product_groups/:id"),
+  getQueries: get("/queries"),
+  getQuery: get("/queries/:id"),
+  createPayment: post("/pay"),
+  deletePayment: del("/pay/:id")
 };
 
 const modifyError = ({ response }) => ({
-	error: response.statusText,
-	status: response.status
+  error: response.statusText,
+  status: response.status
 });
 
 /**
- * This could be more generic but fuck it
- * @param instance
+ * Creating the method that acts on the right context
  */
-const generateQuery = instance => ({ method, url, body }) => instance({
-	method,
-	url,
-	data: body,
-});
+const createMethodHandler = (request, context) => (idOrBody, dataInput) => {
+  let url, data;
+  const { url: urlTemplate, key, method } = context;
+  const requiresId = urlTemplate.includes(':');
+  const hasId = typeof idOrBody === "string";
+
+  if (requiresId && !hasId) {
+    throw new Error(`This method requires an id but none was given.`);
+  }
+
+  if (hasId) {
+    url = replacePlaceholder(urlTemplate, idOrBody);
+    data = key ? { [key]: dataInput } : dataInput;
+  } else {
+    url = urlTemplate;
+    data = idOrBody;
+  }
+
+  return request({ data, url, method, key })
+};
+
+/**
+ * Passing the required context to create requests from the methods
+ * @param {AxiosInstance} instance
+ */
+const createMethods = (instance) => Object.entries(methods).reduce((all, [functionName, options]) => ({
+  ...all,
+  [functionName]: createMethodHandler(query(instance), options)
+}), {});
+
 
 /**
  * Makes a get request that returns the json response
  *
  * The function is CURRIED to allow reusing the
  * function without having to repeat the first parameter
- *
- * You might want to change this up to make it more
- * generic so it can support more than just GET
- * @param {AxiosInstance} instance
- * @returns {function(string): Promise<AxiosResponse>}
  */
-const query = instance => (options) => {
-	const action = generateQuery(instance);
-	return action(options)
-		.then(res => res.data)
-		.catch(e => {
-			throw modifyError(e)
-		});
-};
+const query = instance => (options) => instance(options)
+  .then(res => res.data)
+  .catch(e => {
+    throw modifyError(e)
+  });
 
-/**
- * Creates a selly instance with all the methods
- * attached
- * @param {string} email
- * @param {string} key
- * @param {Object} [options={}] Selly options
- */
 const create = (email, key, options = {}) => {
-	// TODO: Check to make sure email and key aren't empty first
-	const authorization = encode64(`${email}:${key}`);
+  if (!email) {
+    throw new Error("An email is required for selly");
+  }
 
-	const selly = axios.create({
-		baseURL: "https://selly.gg/api/v2/",
-		headers: {
-			Authorization: `Basic ${authorization}`,
-			"User-Agent": options.userAgent || "selly.js"
-		}
-	});
+  if (!key) {
+    throw new Error("An api key is required for selly, grab one at https://selly.gg/settings");
+  }
 
+  const authorization = encode64(`${email}:${key}`);
 
-	const request = query(selly);
+  const selly = axios.create({
+    baseURL: "https://selly.gg/api/v2/",
+    headers: {
+      Authorization: `Basic ${authorization}`,
+      "User-Agent": options.userAgent || "selly.js"
+    }
+  });
 
-	return Object.entries(methods).reduce((all, [methodName, endpoint]) => {
-		const method = (idOrBody, bodyInput) => {
-			let url, body;
-			const { method, key } = endpoint;
-			const requiresId = endpoint.url.includes(':');
-			const hasId = typeof idOrBody === "string";
-
-			if (requiresId && !hasId) {
-				return Promise.reject(`The method ${methodName} requires an id but none was given.`);
-			}
-
-			if (hasId) {
-				url = replacePlaceholder(endpoint.url, idOrBody);
-				body = endpoint.key ? { [endpoint.key]: bodyInput } : bodyInput;
-			} else {
-				url = endpoint.url;
-				body = idOrBody;
-			}
-
-			return request({ body, url, method, key })
-		};
-
-		return {
-			...all,
-			[methodName]: method
-		};
-	}, {});
+  return createMethods(selly);
 };
 
 module.exports = { create };
